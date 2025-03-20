ac9164ff0a72a6a270b750e2f52b4e9e85418ad7
PedroPassos87 <peagacarreira@gmail.com>
2025-03-20 15:57:09 -0300

    tratando arquivos

--- api_rest/src/config/multerConfig.js
+++ api_rest/src/config/multerConfig.js
@@ -4,13 +4,24 @@ import { extname, resolve } from 'path';
 const random = () => Math.floor(Math.random() * 10000 + 10000);
 
 export default {
-  storage: multer.diskStorage({
-    destination: (req, file, cb) => {
-      cb(null, resolve(__dirname, '..', '..', 'uploads'));
-    },
-    filename: (req, file, cb) => {
-      cb(null, `${Date.now()}_${random()}${extname(file.originalname)}`)
-    },
+  fileFilter: (req, file, cb) => {
+    if(file.mimetype !== 'image/png' && file.mimetype !== 'image/jpeg'){
+      return cb(new multer.MulterError('Arquivo inválido. Imagens somente png e jpg'))
+    }
+
+    return cb(null, true);
+
   },
-),
+
+  storage: multer.diskStorage(
+    {
+      destination: (req, file, cb) => {
+        cb(null, resolve(__dirname, '..', '..', 'uploads'));
+      },
+
+      filename: (req, file, cb) => {
+        cb(null, `${Date.now()}_${random()}${extname(file.originalname)}`)
+      },
+    },
+  ),
 };
