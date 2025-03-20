d0c6ced460b703a859b4e5312ef2e37f33c5943d
PedroPassos87 <peagacarreira@gmail.com>
2025-03-20 14:07:43 -0300

    rota alunos

--- api_rest/src/controllers/HomeController.js
+++ api_rest/src/controllers/HomeController.js
@@ -2,16 +2,7 @@ import Aluno from "../models/Aluno";
 
 class HomeController {
   async index(req, res) {
-    const novoAluno = await Aluno.create({
-      nome: "Giovanna",
-      sobrenome: "Passos",
-      email: "gi@gmail.com",
-      idade: 17,
-      peso: 45,
-      altura: 1.65
-
-    });
-    res.json({novoAluno});
+    res.json('index');
   }
 }
 
